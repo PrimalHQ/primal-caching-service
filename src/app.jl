bed10596759c0104db7cdc796034f575e87d7eed
pritk <pradovic@itekako.com>
2023-09-28 02:02:29 +0000

    hidding DM contacts will be done differently

--- src/app.jl
+++ src/app.jl
@@ -665,8 +665,6 @@ function get_directmsg_contacts(
 
         peer = Nostr.PubKeyId(peer)
 
-        is_hidden(est, user_pubkey, :content, peer) && continue
-
         if relation != :any
             if relation == :follows; peer in fs || continue
             elseif relation == :other; peer in fs && continue
